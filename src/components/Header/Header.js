c6942c5142042dbb6497b562b5ddce9c5ddf1fc1
Noreon-Front <foodberen@gmail.com>
2021-11-22 21:15:56 +0600

    Navbar

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,16 +5,17 @@ import Toolbar from "@material-ui/core/Toolbar";
 import IconButton from "@material-ui/core/IconButton";
 import Typography from "@material-ui/core/Typography";
 import InputBase from "@material-ui/core/InputBase";
-import Badge from "@material-ui/core/Badge";
+// import Badge from "@material-ui/core/Badge";
 import MenuItem from "@material-ui/core/MenuItem";
 import Menu from "@material-ui/core/Menu";
 import MenuIcon from "@material-ui/icons/Menu";
 import SearchIcon from "@material-ui/icons/Search";
-import AccountCircle from "@material-ui/icons/AccountCircle";
-import MailIcon from "@material-ui/icons/Mail";
-import NotificationsIcon from "@material-ui/icons/Notifications";
+// import AccountCircle from "@material-ui/icons/AccountCircle";
+// import MailIcon from "@material-ui/icons/Mail";
+// import NotificationsIcon from "@material-ui/icons/Notifications";
 import MoreIcon from "@material-ui/icons/MoreVert";
-import ShoppingCartIcon from "@material-ui/icons/ShoppingCart";
+// import ShoppingCartIcon from "@material-ui/icons/ShoppingCart";
+import "./Header.css";
 
 const useStyles = makeStyles((theme) => ({
   grow: {
@@ -138,7 +139,7 @@ export default function Header() {
 
   return (
     <div className={classes.grow}>
-      <AppBar position="static" color="secondary">
+      <AppBar position="static" className="header">
         <Toolbar>
           <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="open drawer">
             <MenuIcon />
---
+++ src/components/MainPageCom/MainPageCom.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./MainPageCom.css";
+
+const MainPageCom = () => {
+  return (
+    <>
+      <div className="bgc">
+        <div className="inner-div">
+          <h1 className="title">Quantum Houses</h1>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default MainPageCom;
